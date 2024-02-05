c31aacb8b0ef2f8e71b8d8bf2273725c8eef3904
Battatt <151041544+Battatt@users.noreply.github.com>
2024-02-05 18:27:31 +0300

    Task №4

--- 05_02/main.py
+++ 05_02/main.py
@@ -32,5 +32,36 @@ def number_four_index():
                                                  f' alt="not found"></body></html>')
 
 
+@app.route('/promotion_image')
+def promotion_image():
+    prom = (['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+                         'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!',
+                         'Присоединяйся!'])
+    counter = ['secondary', 'primary', 'success', 'info', 'danger']
+    promoute = ''
+    for i in range(len(prom)):
+        promoute += f'<div class="alert alert-{counter[i]}" role="alert">'
+        promoute += prom[i] + '</div></br>'
+    res = f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" 
+                    href="{url_for('static', filename='css/style.css')}" />
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Колонизация</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/Mars.png')}" alt="not found">
+                    {promoute}
+                  </body>
+                  </html>"""
+    return res
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
