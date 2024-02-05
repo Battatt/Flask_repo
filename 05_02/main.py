8fd8b5e198188305497708b1c4b59cec2f18cafa
Battatt <151041544+Battatt@users.noreply.github.com>
2024-02-05 17:19:03 +0300

    Task №3

--- 05_02/main.py
+++ 05_02/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -18,6 +18,18 @@ def third_index():
     return "</br>".join(['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
                          'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!',
                          'Присоединяйся!'])
+
+
+@app.route('/image_mars')
+def number_four_index():
+    return (('<!doctype html>'
+             '<html lang="en">'
+             '<head><meta charset="utf-8">'
+             '<title>Привет, Марс!</title>'
+             '</head>'
+             '<body>'
+             '<h1><i>Жди нас, Марс!</i></h1>') + f'<img src="{url_for('static', filename='img/Mars.png')}"'
+                                                 f' alt="not found"></body></html>')
 
 
 if __name__ == '__main__':
