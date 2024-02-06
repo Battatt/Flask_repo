a7a008ea6519e88a93551ff30658d154ece5a03f
Battatt <151041544+Battatt@users.noreply.github.com>
2024-02-06 20:38:18 +0300

    Task №6

--- 05_02/main.py
+++ 05_02/main.py
@@ -157,6 +157,32 @@ def form_sample():
             return "Форма отправлена"
 
 
+@app.route('/choice/<planet_name>')
+def greeting(planet_name):
+    prom = ['На ней много необходимых ресурсов;', 'На ней есть вода и атмосфера;',
+            'На ней есть небольшое магнитное поле;', 'Наконец, она просто красива!']
+    counter = ['success', 'secondary', 'primary', 'danger']
+    info = ''
+    for i in range(len(prom)):
+        info += f'<div class="alert alert-{counter[i]}" role="alert">'
+        info += prom[i] + '</div>'
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Варианты выбора</title>
+                  </head>
+                  <body>
+                    <h1>Мое предложение: {planet_name}</h1>
+                    <h2>Эта планета близка к земле;</h2>{info}
+                  </body>
+                </html>'''
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
