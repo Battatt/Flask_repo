052452a480e3dbccc9083294826a8690af651f56
Battatt <151041544+Battatt@users.noreply.github.com>
2024-02-06 20:53:39 +0300

    Task №7

--- 05_02/main.py
+++ 05_02/main.py
@@ -184,5 +184,28 @@ def greeting(planet_name):
                 </html>'''
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def result(nickname, level, rating):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Результаты</title>
+                  </head>
+                  <body>
+                    <h1>Результаты отбора</h1>
+                    <h2>Претендента на участие в миссии {str(nickname)}:</h2>
+                    <div class="alert alert-success" role="alert">Поздравляем! Ваш рейтинг после {str(level)} этапа отбора</div>
+                    <h3>Составляет {str(rating)}!</h3>
+                    <div class="alert alert-danger" role="alert">Желаем удачи!</div>
+                  </body>
+                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
