0913c3e3f9745a39f3d98818efb120dd218c60e1
Battatt <151041544+Battatt@users.noreply.github.com>
2024-02-08 16:47:21 +0300

    Task №1 08_02

--- 05_02/main.py
+++ 05_02/main.py
@@ -206,6 +206,25 @@ def result(nickname, level, rating):
                   </body>
                 </html>'''
 
+@app.route('/form_sample', methods=['POST', 'GET'])
+def form_sample():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Пример формы</title>
+                          </head>
+                          <body>
+                          </body>
+                          </html>'''
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
