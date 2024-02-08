c23e2ba9ad3a63d51c3da757ffa2c960f12b1203
Battatt <151041544+Battatt@users.noreply.github.com>
2024-02-08 17:16:30 +0300

    Тренировки в полёте

--- 08_02/01/tomain.py
+++ 08_02/01/tomain.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
@@ -8,6 +8,15 @@ app = Flask(__name__)
 @app.route("/index/<title>")
 def first_index(title):
     return render_template('base.html', title=title)
+
+
+@app.route("/training/<prof>")
+def second_index(prof):
+    if 'ИНЖЕНЕР' in prof.upper() or 'СТРОИТЕЛ' in prof.upper():
+        prof = 0
+    else:
+        prof = 1
+    return render_template('prof.html', prof=prof, title="Профессия")
 
 
 if __name__ == '__main__':
