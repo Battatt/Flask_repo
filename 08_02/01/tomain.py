8470f451fd0e932e4315697039a6a521ca7b8780
Battatt <151041544+Battatt@users.noreply.github.com>
2024-02-10 14:11:12 +0300

    Список профессий

--- 08_02/01/tomain.py
+++ 08_02/01/tomain.py
@@ -19,5 +19,16 @@ def second_index(prof):
     return render_template('prof.html', prof=prof, title="Профессия")
 
 
+@app.route("/list_prof/<list>")
+def list_index(list):
+    l_type = 0 if list == 'ul' else 1 if list == 'ol' else -1
+    professions = [
+        'инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию',
+        'климатолог', 'специалист по радиационной защите', 'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения',
+        'метеоролог', 'оператор марсохода', 'киберинженер', 'штурман', 'пилот дронов'
+    ]
+    return render_template('list_prof.html', list_type=l_type, profs=professions)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
