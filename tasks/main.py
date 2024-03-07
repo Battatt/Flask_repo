7568fac780782725b364b8055c8a7615b6117061
lyceum <battamaev@yandex.ru>
2024-03-07 16:28:59 +0300

    07 march update

--- tasks/main.py
+++ tasks/main.py
@@ -1,14 +1,14 @@
 from requests import get, post, delete
 
-print(get('http://localhost:5000/api/users').json())
-print(get('http://localhost:5000/api/users/1').json())
+print(get('http://localhost:5000/api/v2/users').json())
+print(get('http://localhost:5000/api/v2/users/1').json())
 
-print(post('http://localhost:5000/api/users', json={}).json())
+print(post('http://localhost:5000/api/v2/users', json={}).json())
 
-print(post('http://localhost:5000/api/users',
+print(post('http://localhost:5000/api/v2/users',
            json={'name': 'Юлия'}).json())
 
-print(post('http://localhost:5000/api/users',
+print(post('http://localhost:5000/api/v2/users',
            json={'surname': 'Фамилия',
                  'name': 'Имя',
                  'age': 10,
@@ -18,7 +18,7 @@ print(post('http://localhost:5000/api/users',
                  'email': "school17@mail.ru",
                  'hashed_password': "A2udh7y381h!"}).json())
 
-print(delete('http://localhost:5000/api/users/999').json())
+print(delete('http://localhost:5000/api/v2/users').json())
 # user с id = 999 нет в базе
 
-print(delete('http://localhost:5000/api/users/1').json())
+print(delete('http://localhost:5000/api/v2/users').json()
